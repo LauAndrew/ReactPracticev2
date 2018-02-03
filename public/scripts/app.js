a6bc9a3457b964085194570d9685da1661cbcfc3
LauAndrew <lau.Andrew92@gmail.com>
2018-02-02 17:41:36 -0800

    Added additional Class Practice

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -25,7 +27,12 @@ var Person = function () {
     _createClass(Person, [{
         key: 'getGreeting',
         value: function getGreeting() {
-            return 'Hi this is ' + this.name + ' and I am ' + this.age + ' year(s) old';
+            return 'Hi this is ' + this.name + '.';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return 'This is ' + this.name + ' and I am ' + this.age + ' years old. ';
         }
     }]);
 
@@ -35,9 +42,7 @@ var Person = function () {
 var Student = function (_Person) {
     _inherits(Student, _Person);
 
-    function Student(name, age) {
-        var major = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "undefined";
-
+    function Student(name, age, major) {
         _classCallCheck(this, Student);
 
         var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
@@ -46,11 +51,60 @@ var Student = function (_Person) {
         return _this;
     }
 
+    _createClass(Student, [{
+        key: 'hasMajor',
+        value: function hasMajor() {
+            return !!this.major;
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+            if (this.hasMajor()) {
+                description += 'There major is ' + this.major + '.';
+            }
+            return description;
+        }
+    }]);
+
     return Student;
 }(Person);
 
-var me = new Student('Andrew Lau', 25, 'Computer Science');
-console.log(me);
+var Traveler = function (_Person2) {
+    _inherits(Traveler, _Person2);
 
-var other = new Student();
-console.log(other);
+    function Traveler(name, age, location) {
+        _classCallCheck(this, Traveler);
+
+        var _this2 = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age));
+
+        _this2.location = location;
+        return _this2;
+    }
+
+    _createClass(Traveler, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            var greeting = _get(Traveler.prototype.__proto__ || Object.getPrototypeOf(Traveler.prototype), 'getGreeting', this).call(this);
+            if (this.location) {
+                greeting += ' I\'m visiting from ' + this.location + '.';
+            }
+            return greeting;
+        }
+    }]);
+
+    return Traveler;
+}(Person);
+
+// Traveler -> Person
+// Add suport for homeLocation
+// Override getGreeting
+// 1. Hi. I am Andrew Lau. I'm visiting from Walnut Creek.
+// 2. Hi. I am Andrew Lau
+
+
+var me = new Traveler('Andrew Lau', 25, 'Walnut Creek');
+console.log(me.getGreeting());
+
+var other = new Traveler();
+console.log(other.getGreeting());
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -7,20 +7,55 @@ class Person{
         this.age = age;
     }
     getGreeting() {
-        return `Hi this is ${this.name} and I am ${this.age} year(s) old`;
+        return `Hi this is ${this.name}.`;
+    }
+    getDescription() {
+        return `This is ${this.name} and I am ${this.age} years old. `;
     }
 }
 
 class Student extends Person{
-    constructor(name, age, major = "undefined"){
+    constructor(name, age, major){
         super(name, age);
         this.major = major
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription(){
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += `There major is ${this.major}.`;
+        }
+        return description;
     }
 
 }
 
-const me = new Student('Andrew Lau', 25, 'Computer Science');
-console.log(me);
+class Traveler extends Person {
+    constructor(name, age, location){
+        super(name, age);
+        this.location = location;
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+        if (this.location) {
+            greeting += ` I'm visiting from ${this.location}.`;
+        }
+        return greeting;
+    }
 
-const other = new Student();
-console.log(other);
+}
+
+// Traveler -> Person
+// Add suport for homeLocation
+// Override getGreeting
+// 1. Hi. I am Andrew Lau. I'm visiting from Walnut Creek.
+// 2. Hi. I am Andrew Lau
+
+
+const me = new Traveler('Andrew Lau', 25, 'Walnut Creek');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
